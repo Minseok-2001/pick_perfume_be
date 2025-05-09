f197e5b23e4eb25a6ee0a5bd08354abf82c75abf
정민석 <ms.jung.dev@gmail.com>
2025-05-10 04:17:22 +0900

    feat: Add MemberController for managing member-related operations

---
+++ src/main/kotlin/ym_cosmetic/pick_perfume_be/member/conrtroller/MemberController.kt
@@ -0,0 +1,28 @@
+package ym_cosmetic.pick_perfume_be.member.conrtroller
+
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.*
+import ym_cosmetic.pick_perfume_be.common.dto.response.ApiResponse
+import ym_cosmetic.pick_perfume_be.member.MemberService
+import ym_cosmetic.pick_perfume_be.member.dto.MemberResponse
+import ym_cosmetic.pick_perfume_be.member.dto.SignupRequest
+import ym_cosmetic.pick_perfume_be.member.entity.Member
+import ym_cosmetic.pick_perfume_be.security.CurrentMember
+
+
+@RestController
+@RequestMapping("/members")
+class MemberController(
+    private val memberService: MemberService
+) {
+
+    @GetMapping("/me")
+    fun getCurrentMember(@CurrentMember currentMember: Member): ApiResponse<MemberResponse> {
+        return ApiResponse.success(MemberResponse.from(currentMember))
+    }
+
+    @PostMapping()
+    fun createMember(@Valid @RequestBody dto: SignupRequest): ApiResponse<MemberResponse> {
+        return ApiResponse.success(memberService.createMember(dto))
+    }
+}
