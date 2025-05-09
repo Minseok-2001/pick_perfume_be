3d234b9b843b3137d2d1b06519278488b901310e
정민석 <ms.jung.dev@gmail.com>
2025-05-10 04:17:31 +0900

    feat: Add MemberResponse data class for member data transfer

---
+++ src/main/kotlin/ym_cosmetic/pick_perfume_be/member/dto/MemberResponse.kt
@@ -0,0 +1,25 @@
+package ym_cosmetic.pick_perfume_be.member.dto
+
+import ym_cosmetic.pick_perfume_be.common.vo.ImageUrl
+import ym_cosmetic.pick_perfume_be.member.entity.Member
+import java.time.LocalDateTime
+
+data class MemberResponse(
+    val id: Long,
+    val email: String,
+    val nickname: String,
+    val profileImage: ImageUrl?,
+    val createdAt: LocalDateTime,
+) {
+    companion object {
+        fun from(member: Member): MemberResponse {
+            return MemberResponse(
+                id = member.id!!,
+                email = member.email,
+                nickname = member.nickname,
+                profileImage = member.profileImage,
+                createdAt = member.createdAt,
+            )
+        }
+    }
+}
