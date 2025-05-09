0a1990431cf8a8c84632287e08adaa5620b8b65b
정민석 <ms.jung.dev@gmail.com>
2025-05-10 04:17:39 +0900

    feat: Implement createMember method in MemberService for member registration

--- src/main/kotlin/ym_cosmetic/pick_perfume_be/member/MemberService.kt
+++ src/main/kotlin/ym_cosmetic/pick_perfume_be/member/MemberService.kt
@@ -1,16 +1,38 @@
 package ym_cosmetic.pick_perfume_be.member
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import ym_cosmetic.pick_perfume_be.common.exception.EntityNotFoundException
+import ym_cosmetic.pick_perfume_be.member.dto.MemberResponse
+import ym_cosmetic.pick_perfume_be.member.dto.SignupRequest
 import ym_cosmetic.pick_perfume_be.member.entity.Member
 import ym_cosmetic.pick_perfume_be.member.repository.MemberRepository
+import ym_cosmetic.pick_perfume_be.security.PasswordEncoder
 
 @Service
-class MemberService(private val memberRepository: MemberRepository) {
+class MemberService(
+    private val memberRepository: MemberRepository,
+    private val passwordEncoder: PasswordEncoder
+) {
 
+
+    @Transactional(readOnly = true)
     fun findById(memberId: Long): Member {
         return memberRepository.findById(memberId).orElseThrow {
             throw EntityNotFoundException("Member not found with id: $memberId")
         }
+    }
+
+    @Transactional
+    fun createMember(dto: SignupRequest): MemberResponse {
+        val member = Member(
+            nickname = dto.nickname,
+            name = dto.name,
+            password = dto.password?.let { passwordEncoder.encode(it) },
+            email = dto.email,
+            profileImage = dto.profileImage
+        )
+        val savedMember = memberRepository.save(member)
+        return MemberResponse.from(savedMember)
     }
 }
