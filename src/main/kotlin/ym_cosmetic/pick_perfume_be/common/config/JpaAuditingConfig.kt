e45204d6f3b0c13cb1ac0025b1112dae1fe0d033
정민석 <ms.jung.dev@gmail.com>
2025-05-09 19:24:46 +0900

    feat: Add JpaAuditingConfig for enabling JPA auditing

---
+++ src/main/kotlin/ym_cosmetic/pick_perfume_be/common/config/JpaAuditingConfig.kt
@@ -0,0 +1,10 @@
+package ym_cosmetic.pick_perfume_be.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+
+}
