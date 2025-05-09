07aa2d21a198dd5519d6f5863dc13122c7b1ab36
정민석 <ms.jung.dev@gmail.com>
2025-05-09 19:26:05 +0900

    feat: Add PerfumeNoteRepository for managing PerfumeNote entities

---
+++ src/main/kotlin/ym_cosmetic/pick_perfume_be/perfume/repository/PerfumeNoteRepository.kt
@@ -0,0 +1,21 @@
+package ym_cosmetic.pick_perfume_be.perfume.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import ym_cosmetic.pick_perfume_be.perfume.entity.PerfumeNote
+import ym_cosmetic.pick_perfume_be.perfume.vo.NoteType
+
+interface PerfumeNoteRepository : JpaRepository<PerfumeNote, Long> {
+    fun findByPerfumeId(perfumeId: Long): List<PerfumeNote>
+
+    fun findByPerfumeIdAndType(perfumeId: Long, type: NoteType): List<PerfumeNote>
+
+    @Modifying
+    @Query("DELETE FROM PerfumeNote pn WHERE pn.perfume.id = :perfumeId")
+    fun deleteByPerfumeId(perfumeId: Long)
+
+    @Modifying
+    @Query("DELETE FROM PerfumeNote pn WHERE pn.perfume.id = :perfumeId AND pn.type = :type")
+    fun deleteByPerfumeIdAndType(perfumeId: Long, type: NoteType)
+}
