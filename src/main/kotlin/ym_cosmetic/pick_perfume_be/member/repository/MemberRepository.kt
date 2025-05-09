c4648630030281d74961d8387b2fb11cdd37915e
정민석 <ms.jung.dev@gmail.com>
2025-05-09 19:24:54 +0900

    feat: Add MemberRepository interface for Member entity management

---
+++ src/main/kotlin/ym_cosmetic/pick_perfume_be/member/repository/MemberRepository.kt
@@ -0,0 +1,7 @@
+package ym_cosmetic.pick_perfume_be.member.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import ym_cosmetic.pick_perfume_be.member.entity.Member
+
+
+interface MemberRepository : JpaRepository<Member, Long>
