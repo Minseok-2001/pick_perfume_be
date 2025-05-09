db2755029eed91552837b3a918811a67f2077386
정민석 <ms.jung.dev@gmail.com>
2025-05-10 04:17:26 +0900

    feat: Add email-based query methods to MemberRepository

--- src/main/kotlin/ym_cosmetic/pick_perfume_be/member/repository/MemberRepository.kt
+++ src/main/kotlin/ym_cosmetic/pick_perfume_be/member/repository/MemberRepository.kt
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import ym_cosmetic.pick_perfume_be.member.entity.Member
 
 
-interface MemberRepository : JpaRepository<Member, Long>
+interface MemberRepository : JpaRepository<Member, Long> {
+    fun findByEmail(email: String): Member?
+    fun existsByEmail(email: String): Boolean
+}
