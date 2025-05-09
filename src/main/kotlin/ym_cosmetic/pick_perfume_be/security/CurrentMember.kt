57abc69b6996157048806e48bc17c24c9c203bcc
정민석 <ms.jung.dev@gmail.com>
2025-05-10 04:16:40 +0900

    feat: Add Swagger annotation to hide CurrentMember parameter in API documentation

--- src/main/kotlin/ym_cosmetic/pick_perfume_be/security/CurrentMember.kt
+++ src/main/kotlin/ym_cosmetic/pick_perfume_be/security/CurrentMember.kt
@@ -1,5 +1,8 @@
 package ym_cosmetic.pick_perfume_be.security
 
+import io.swagger.v3.oas.annotations.Parameter
+
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.RUNTIME)
+@Parameter(hidden = true)
 annotation class CurrentMember
