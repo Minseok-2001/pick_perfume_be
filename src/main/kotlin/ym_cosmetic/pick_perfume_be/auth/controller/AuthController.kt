dc8916e28e51e3f4d10da9a45e579a17dd8128fe
정민석 <ms.jung.dev@gmail.com>
2025-05-10 04:16:26 +0900

    feat: Implement login functionality with request validation and response handling

--- src/main/kotlin/ym_cosmetic/pick_perfume_be/auth/controller/AuthController.kt
+++ src/main/kotlin/ym_cosmetic/pick_perfume_be/auth/controller/AuthController.kt
@@ -1,24 +1,32 @@
 package ym_cosmetic.pick_perfume_be.auth.controller
 
+import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import ym_cosmetic.pick_perfume_be.auth.dto.LoginRequest
+import ym_cosmetic.pick_perfume_be.auth.dto.LoginResponse
 import ym_cosmetic.pick_perfume_be.auth.service.AuthService
+import ym_cosmetic.pick_perfume_be.common.dto.response.ApiResponse
 
 @RestController
 @RequestMapping("/auth")
-class AuthController {
-
-    private val authService: AuthService? = null
+class AuthController(
+    private val authService: AuthService
+) {
 
 
     @PostMapping("/login")
-    fun login() {
-        // 로그인 처리 로직
+    fun login(
+        @RequestBody request: @Valid LoginRequest
+    ): ApiResponse<LoginResponse?> {
+        val response: LoginResponse? = authService.login(request)
+        return ApiResponse.success(response)
     }
 
     @PostMapping("/logout")
     fun logout() {
-        // 로그아웃 처리 로직
+        authService.logout()
     }
 }
