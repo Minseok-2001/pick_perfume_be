b36a735f385e14f3693260df81086ecc213f0b8f
정민석 <ms.jung.dev@gmail.com>
2025-05-10 01:26:27 +0900

    refactor: Rename userPreferences parameter to memberPreference in findRecommendedPerfumesByMemberPreferences method

--- src/main/kotlin/ym_cosmetic/pick_perfume_be/search/repository/PerfumeSearchRepository.kt
+++ src/main/kotlin/ym_cosmetic/pick_perfume_be/search/repository/PerfumeSearchRepository.kt
@@ -1,6 +1,7 @@
 package ym_cosmetic.pick_perfume_be.search.repository
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
+import ym_cosmetic.pick_perfume_be.member.entity.MemberPreference
 import ym_cosmetic.pick_perfume_be.search.document.PerfumeDocument
 import ym_cosmetic.pick_perfume_be.search.dto.PerfumeSearchCriteria
 
@@ -18,8 +19,8 @@ interface PerfumeSearchRepository : ElasticsearchRepository<PerfumeDocument, Str
 interface PerfumeSearchRepositoryCustom {
     fun search(searchCriteria: PerfumeSearchCriteria): List<PerfumeDocument>
     fun findSimilarPerfumes(perfumeId: String, limit: Int): List<PerfumeDocument>
-    fun findRecommendedPerfumesByUserPreferences(
-        userPreferences: UserPreferences,
+    fun findRecommendedPerfumesByMemberPreferences(
+        memberPreference: MemberPreference,
         limit: Int
     ): List<PerfumeDocument>
 }
