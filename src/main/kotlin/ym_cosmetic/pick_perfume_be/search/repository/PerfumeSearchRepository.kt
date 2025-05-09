f2a8ce7ae8a6a9337091a1a3763c229bf95614e1
정민석 <ms.jung.dev@gmail.com>
2025-05-10 04:17:49 +0900

    refactor: Simplify PerfumeSearchRepository by removing unused query methods

--- src/main/kotlin/ym_cosmetic/pick_perfume_be/search/repository/PerfumeSearchRepository.kt
+++ src/main/kotlin/ym_cosmetic/pick_perfume_be/search/repository/PerfumeSearchRepository.kt
@@ -4,13 +4,5 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import ym_cosmetic.pick_perfume_be.search.document.PerfumeDocument
 
 interface PerfumeSearchRepository : ElasticsearchRepository<PerfumeDocument, String>,
-    PerfumeSearchRepositoryCustom {
-    fun findByBrandName(brandName: String): List<PerfumeDocument>
-    fun findByNotesByTypeTypeAndNotesByTypeNotesContains(
-        type: String,
-        note: String
-    ): List<PerfumeDocument>
-
-    fun findByAccordsContaining(accord: String): List<PerfumeDocument>
-}
+    PerfumeSearchRepositoryCustom
 
