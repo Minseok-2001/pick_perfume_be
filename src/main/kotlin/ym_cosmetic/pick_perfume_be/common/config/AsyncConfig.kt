edf1fd6af2a6df18fbe104d3fb7d7519bee3a397
정민석 <ms.jung.dev@gmail.com>
2025-05-09 23:11:44 +0900

    feat: Add asynchronous and caching configuration for improved performance

---
+++ src/main/kotlin/ym_cosmetic/pick_perfume_be/common/config/AsyncConfig.kt
@@ -0,0 +1,22 @@
+package ym_cosmetic.pick_perfume_be.common.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.util.concurrent.Executor
+
+@Configuration
+@EnableAsync
+class AsyncConfig {
+    @Bean
+    fun taskExecutor(): Executor {
+        val executor = ThreadPoolTaskExecutor()
+        executor.corePoolSize = 5
+        executor.maxPoolSize = 10
+        executor.queueCapacity = 25
+        executor.setThreadNamePrefix("MemberPreference-")
+        executor.initialize()
+        return executor
+    }
+}
