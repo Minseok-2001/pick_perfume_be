f3221b47fb4c3bc6aee34fbf0bade6b9c10063c1
정민석 <ms.jung.dev@gmail.com>
2025-09-12 01:22:29 +0900

    Add OpenAPI server definitions and clean up Swagger config
    Introduced @OpenAPIDefinition with server URLs for production and local development in OpenApiConfig. Removed redundant server-url and related properties from application.yml to centralize server configuration in code.

--- src/main/kotlin/ym_cosmetic/pick_perfume_be/common/config/OpenApiConfig.kt
+++ src/main/kotlin/ym_cosmetic/pick_perfume_be/common/config/OpenApiConfig.kt
@@ -1,5 +1,7 @@
 package ym_cosmetic.pick_perfume_be.common.config
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
@@ -10,6 +12,12 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@OpenAPIDefinition(
+    servers = [
+        Server(url = "https://api.scentist.link", description = "Production server"),
+        Server(url = "http://localhost:8080", description = "Local development server")
+    ]
+)
 class OpenApiConfig {
 
     @Value("\${SWAGGER_USERNAME:admin}")
